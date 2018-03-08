cd85dbe4dbff3e17cb6c7d19dfb081944eecf746
Sukhorukov Maksim <sukhorukov.mv@reliab.tech>
2018-03-08 18:16:09 +0300

    add test

--- test/chapter1/test-1.10.rkt
+++ test/chapter1/test-1.10.rkt
@@ -5,4 +5,3 @@
 (check-equal? (A 1 10) 1024)
 (check-equal? (A 2 4) 65536)
 (check-equal? (A 3 3) 65536)
-(print "test passed")
---
+++ test/chapter1/test-1.3.rkt
@@ -0,0 +1,9 @@
+#lang racket/base
+
+(require rackunit "../../src/chapter1/1.3.rkt")
+
+(check-equal? (sum-of-squares-of-top-two 4 6 9) 117 "test 1")
+(check-equal? (sum-of-squares-of-top-two 5 2 3) 34 "test 2")
+(check-equal? (sum-of-squares-of-top-two 34 67 21) 5645 "test 3")
+(check-equal? (sum-of-squares-of-top-two 234 1213 9913) 99738938 "test 4")
+(check-equal? (sum-of-squares-of-top-two 4 9 10) 181 "test 5")
