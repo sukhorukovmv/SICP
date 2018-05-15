9d7e48a3b066aed604e9dc02cadb3a30936fc158
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2018-05-15 21:56:14 +0300

    Fix test 1.19

--- test/chapter1/test-1.19.rkt
+++ test/chapter1/test-1.19.rkt
@@ -2,7 +2,7 @@
 
 (require rackunit "../../src/chapter1/1.19.rkt")
 
-(check-equal? (fib 1) 4 "test 1")
-(check-equal? (fib 2) 2 "test 2")
-(check-equal? (fib 5) 7 "test 3")
+(check-equal? (fib 1) 1 "test 1")
+(check-equal? (fib 2) 1 "test 2")
+(check-equal? (fib 5) 5 "test 3")
 (check-equal? (fib 6) 8 "test 4")
