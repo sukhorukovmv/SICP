d7ecefd97e413aca74ec884dfd7cfc28ff197b38
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2018-05-16 21:40:35 +0300

    Add 1.22

--- src/chapter1/1.22.rkt
+++ src/chapter1/1.22.rkt
@@ -1,18 +1,27 @@
 #lang racket
-(require "./smallest-divisor.rkt")
+(require "./1.21.rkt")
 
 (define (prime? n)
   (= n  (smallest-divisor n)))
 
 (define (runtime)          
-  (current-inexact-milliseconds)) 
-(define (timed-prime-test n)
-  (newline)              
-  (display n)            
-  (start-prime-test n (runtime))) 
-(define (start-prime-test n start-time) 
-  (cond ((prime? n) (report-prime (- (runtime) start-time)))))
+  (current-inexact-milliseconds))
 
-(define (report-prime elapsed-time)
+;(define (timed-prime-test n)           
+;  (start-prime-test n (runtime)))
+
+(define (report-prime n elapsed-time)
+  (newline)
+  (display n)
   (display " *** ")      
   (display elapsed-time))
+
+(define (start-prime-test n start-time)
+  (cond ((prime? n)(report-prime n  (- (runtime) start-time))
+         n)
+        (else (start-prime-test (+ n 1) start-time))))
+
+(define (search-for-primes n count)
+  (cond ((> count 0) (search-for-primes (+ (start-prime-test n (runtime)) 1) (- count 1)))))
+
+
